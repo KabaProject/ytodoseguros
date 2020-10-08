12cbe2f68a49acf34d6ce50be15f2943c23b6e04
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-10-07 22:50:42 -0500

    insurance with tab event

--- src/js/insurance.js
+++ src/js/insurance.js
@@ -1,16 +1,26 @@
 const tabs = document.getElementsByClassName("tab");
+const insDescrs = document.getElementsByClassName("ins-desc");
 
 tabOnClick(tabs[0]);
+insDescrShow(insDescrs[0]);
 
 for(let i = 0; i < tabs.length; i++){
-	addTabEvents(tabs[i], tabs);
+	addTabEvents({
+		tab: tabs[i],
+		tabs: tabs,
+		insDescr: insDescrs[i],
+		insDescrs: insDescrs
+	});
 }
 
-function addTabEvents(tab, tabs){
+function addTabEvents({tab, tabs, insDescr, insDescrs}){
 	tab.addEventListener("click", function(e){
 
 		tabToNormal(tabs);
 		tabOnClick(tab);
+		
+		insDescrHide(insDescrs);
+		insDescrShow(insDescr);
 	});
 	return false;
 }
@@ -27,10 +37,24 @@ function tabOnClick(tab){
 	}
 }
 
+function insDescrShow(insDescr){
+	const styles = insDescr.style;
+	styles.display = 'flex';
+
+}
+
 function tabToNormal(tabs){
 	for(let i = 0; i < tabs.length; i++){
 		if (tabs[i].style.animationName === 'tab-normal-active'){
 			tabs[i].style.animationName = 'tab-active-normal';
+		}
+	}
+}
+
+function insDescrHide(insDescrs){
+	for(let i = 0; i < insDescrs.length; i++){
+		if (insDescrs[i].style.display === 'flex'){
+			insDescrs[i].style.display = 'none';
 		}
 	}
 }
