5a196043de9e8daea10c8379f9acb2b726e7217a
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-10-10 05:19:50 -0500

    animations created

--- src/js/insurance.js
+++ src/js/insurance.js
@@ -1,5 +1,7 @@
 const tabs = document.getElementsByClassName("tab");
 const insDescrs = document.getElementsByClassName("ins-desc");
+const insTl = gsap.timeline();
+
 
 tabOnClick(tabs[0]);
 insDescrShow(insDescrs[0]);
@@ -9,7 +11,7 @@ for(let i = 0; i < tabs.length; i++){
 		tab: tabs[i],
 		tabs: tabs,
 		insDescr: insDescrs[i],
-		insDescrs: insDescrs
+		insDescrs: insDescrs,
 	});
 }
 
@@ -37,10 +39,28 @@ function tabOnClick(tab){
 	}
 }
 
-function insDescrShow(insDescr){
+function insDescrShow(insDescr, isFirst){
 	const styles = insDescr.style;
+	const content = insDescr.getElementsByClassName("ins-desc__content")[0];
+	const image = insDescr.getElementsByClassName("ins-desc__img")[0];
+	
+
 	styles.display = 'flex';
 
+
+	insTl.from(image, {
+			opacity: 0,
+			x: 250,
+			duration: 0.7
+		})
+		.from(content, {
+			opacity: 0,
+			x: -200,
+			duration: 1	
+		});
+
+
+	console.log("clicked");
 }
 
 function tabToNormal(tabs){
@@ -54,6 +74,10 @@ function tabToNormal(tabs){
 function insDescrHide(insDescrs){
 	for(let i = 0; i < insDescrs.length; i++){
 		if (insDescrs[i].style.display === 'flex'){
+
+			const content = insDescrs[i].getElementsByClassName("ins-desc__content")[0];
+			const image = insDescrs[i].getElementsByClassName("ins-desc__img")[0];
+
 			insDescrs[i].style.display = 'none';
 		}
 	}
